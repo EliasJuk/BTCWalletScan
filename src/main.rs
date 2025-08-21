5e92cac1730fa492fecdb789c3e01877e965dcef
eliasjuk <eliasp.juk@hotmail.com>
2025-08-21 17:27:53 -0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use colored::*;
+use std::process::Command;
 
 fn main() {
-  println!("{}","Hello World!".bold().yellow());
+  let output = Command::new("cmd")
+    .args(&["/C", "echo Hello from CMD!"])
+    .output()
+    .expect("falha ao executar comando");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    println!("{} {}","Saída do CMD:".bold().yellow(), stdout);
 }
