ce0c1790146cb69814d90144e81191b3b2be6da8
eliasjuk <eliasp.juk@hotmail.com>
2025-08-22 19:26:01 -0300

    update msg

--- src/utils/msg.rs
+++ src/utils/msg.rs
@@ -1,7 +1,7 @@
 use colored::*;
 
 pub fn msg() {
-    println!("{}", "Certifique-se que o Bitcoin Core está sincronizado e finalizado".bold().yellow());
-    println!("{}", "Use o comando: bitcoind -deprecatedrpc=create_bdb".bold().green());
-    println!("{}", "...".bold().green());
+  println!("{}", "Certifique-se que o Bitcoin Core está sincronizado e finalizado".bold().yellow());
+  println!("{}", "Use o comando: bitcoind -deprecatedrpc=create_bdb".bold().green());
+  println!("{}", "...".bold().green());
 }
