e2b920b24a29df0eafacb222d9945cb13dd3cbd3
eliasjuk <eliasp.juk@hotmail.com>
2025-08-22 17:41:11 -0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
-use colored::*;
-use std::process::Command;
+use std::{thread, time::Duration};
+use crate::utils::{*};
+
+mod utils {
+  pub mod command;
+  pub mod create_legacy_wallet;
+  pub mod list_wallets;
+  pub mod import_wallet;
+  pub mod msg;
+  pub mod load_wallet;
+}
 
 fn main() {
-  let output = Command::new("cmd")
-    .args(&["/C", "echo Hello from CMD!"])
-    .output()
-    .expect("falha ao executar comando");
+    let wallet_name = "wallet_00014";
+    let privkey = "L21AkScUt7anosjfeg6i2oqajvLmAftDVf6jwNjiJ6GpAcynMNEG";
+    let label = "chave_teste";
 
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    println!("{} {}","Saída do CMD:".bold().yellow(), stdout);
+  // MSG
+  msg::msg();
+
+  // Criar carteira Legacy
+  create_legacy_wallet::create_wallet(wallet_name);
+
+  // Pequeno delay para o node estabilizar
+  thread::sleep(Duration::from_secs(2));
+
+  // Listar wallets carregadas e todas no disco
+  list_wallets::list_loaded_wallets();
+  list_wallets::list_all_wallets();
+
+  // Carregar Wallet
+  load_wallet::loadwallet(wallet_name);
+
+  // Pequeno delay para o node estabilizar
+  thread::sleep(Duration::from_secs(2));
+
+  // Importar Chave Privada
+  import_wallet::import_privkey(wallet_name, privkey, label);
 }
