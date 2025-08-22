5c0572550b355ae72346fd5bb9b8fb0a4c4e0cca
eliasjuk <eliasp.juk@hotmail.com>
2025-08-22 19:25:47 -0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,26 @@
-use std::{thread, time::Duration};
 use crate::utils::{*};
 
 mod utils {
-  pub mod command;
-  pub mod create_legacy_wallet;
-  pub mod list_wallets;
-  pub mod import_wallet;
   pub mod msg;
-  pub mod load_wallet;
+  pub mod create_wallet;
+  pub mod import_key;
+  pub mod check_label;
+  pub mod getallinfo;
+  pub mod check_balance;
 }
 
 fn main() {
-    let wallet_name = "wallet_00014";
-    let privkey = "L21AkScUt7anosjfeg6i2oqajvLmAftDVf6jwNjiJ6GpAcynMNEG";
-    let label = "chave_teste";
+  let wallet_name = "wallet_0044";
+  let privkey = "L21AkScUt7anosjfeg6i2oqajvLmAftDVf6jwNjiJ6GpAcynMNEG";
+  let label = "chave_teste";
 
-  // MSG
+  // AVISO!
   msg::msg();
 
-  // Criar carteira Legacy
-  create_legacy_wallet::create_wallet(wallet_name);
-
-  // Pequeno delay para o node estabilizar
-  thread::sleep(Duration::from_secs(2));
-
-  // Listar wallets carregadas e todas no disco
-  list_wallets::list_loaded_wallets();
-  list_wallets::list_all_wallets();
-
-  // Carregar Wallet
-  load_wallet::loadwallet(wallet_name);
-
-  // Pequeno delay para o node estabilizar
-  thread::sleep(Duration::from_secs(2));
-
-  // Importar Chave Privada
-  import_wallet::import_privkey(wallet_name, privkey, label);
+  // SCRIPT!
+  create_wallet::create_wallet(wallet_name);
+  import_key::import_privkey(wallet_name, privkey, label);
+  check_label::get_addresses_by_label(wallet_name, label);
+  getallinfo::get_wallet_info(wallet_name);
+  check_balance::check_balance(wallet_name);
 }
