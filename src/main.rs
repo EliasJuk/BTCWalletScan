330603be843077beec5c8b99e696fe29890f030d
eliasjuk <eliasp.juk@hotmail.com>
2025-08-23 16:23:35 -0300

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,77 @@
 use crate::utils::{*};
+use std::fs::OpenOptions;
+use std::io::Write;
 
 mod utils {
-  pub mod msg;
-  pub mod create_wallet;
-  pub mod import_key;
-  pub mod check_label;
-  pub mod getallinfo;
-  pub mod check_balance;
+	pub mod msg;
+	pub mod create_wallet;
+	pub mod import_key;
+	pub mod check_label;
+	//pub mod getallinfo;
+	pub mod check_balance;
+	pub mod random;
+	pub mod ranges;
+	pub mod unload_delete;
+	pub mod wif_encoder;
 }
 
 fn main() {
-  let wallet_name = "wallet_0044";
-  let privkey = "L21AkScUt7anosjfeg6i2oqajvLmAftDVf6jwNjiJ6GpAcynMNEG";
-  let label = "chave_teste";
+	let wallet_name_base = "wallet_name_";
+	let label_base = "label_";
+	let range = "70"; // ✅ Defina o range fixo aqui
+	let total_loops = 10000; // ✅ Quantas carteiras você quer testar
 
-  // AVISO!
-  msg::msg();
+	msg::msg();
 
-  // SCRIPT!
-  create_wallet::create_wallet(wallet_name);
-  import_key::import_privkey(wallet_name, privkey, label);
-  check_label::get_addresses_by_label(wallet_name, label);
-  getallinfo::get_wallet_info(wallet_name);
-  check_balance::check_balance(wallet_name);
+	for i in 0..total_loops {
+		let wallet_name = format!("{}{:02}", wallet_name_base, i);
+		let label = format!("{}{:02}", label_base, i);
+
+		let privkey = match random::gerar_hex_por_range(range) {
+			Some(hex) => {
+				println!("🔑 Chave HEX gerada: {}", hex);
+				hex
+			}
+			None => {
+				println!("❌ Range inválido: {}", range);
+				continue;
+			}
+		};
+
+		let padded_key = format!("{:0>64}", privkey);
+
+		let wif = match wif_encoder::key_to_wif(&padded_key) {
+			Ok(wif) => wif,
+			Err(e) => {
+				println!("❌ Erro ao converter para WIF: {}", e);
+				continue;
+			}
+		};
+
+		println!("📨 WIF: {}", &wif);
+
+
+		create_wallet::create_wallet(&wallet_name);
+		import_key::import_privkey(&wallet_name, &wif, &label);
+		//check_label::get_addresses_by_label(&wallet_name, &label);
+		//getallinfo::get_wallet_info(&wallet_name);
+
+		let saldo = check_balance::check_balance(&wallet_name);
+		println!("💰 Saldo: {}", saldo);
+
+		if saldo > 0.00000000 {
+			let mut file = OpenOptions::new()
+				.create(true)
+				.append(true)
+				.open("carteiras_com_saldo.txt")
+				.expect("Erro ao abrir/criar o arquivo");
+
+			writeln!(file, "Saldo: {}, WIF: {}", saldo, wif)
+				.expect("Erro ao escrever no arquivo");
+		}
+
+		unload_delete::unload_and_delete_wallet(&wallet_name);
+	}
+
+	println!("✅ Scanner finalizado com {} tentativas.", total_loops);
 }
